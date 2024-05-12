2a4389f05be6700e065e13b2951f4e0b86be14f8
AyrtonF <ayrtonleonardo14@gmail.com>
2024-05-11 22:14:17 -0300

    transferindo arquivos

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { router } from './router';
-import express, {json} from 'express'
-
-const app = express()
-app.use(json())
-app.use(router)
-
-
-app.listen(3333,()=>console.log("Servidor na porta 3333"))
