86f2c76bbd078f72dfb1b826c1a5f505466f31ab
andrefilipe1310 <andrefilipef1310@gmail.com>
2024-05-13 16:20:01 -0300

    adicionando projeto front antigo na main

---
+++ frontend/projetoAntigo/Prototipo tela de login/script.js
@@ -0,0 +1,10 @@
+let loginForm = document.querySelector(".my-form");
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let email = document.getElementById("email");
+    let senha = document.getElementById("senha");
+
+});
+
+
