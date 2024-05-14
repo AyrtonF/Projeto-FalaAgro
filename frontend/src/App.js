6652ca3d7b40325bb74cb20d4acf6ab9115e54e5
andrefilipe1310 <andrefilipef1310@gmail.com>
2024-05-13 22:03:27 -0300

    criando o readme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ frontend/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+
+
+
+function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/favorites" component={FavoritesPage} />
+        <Route path="/chat" component={ChatPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routes;
